75484979d2783b6fb2cfa165724dc070bc9d2c23
satwikakota <68615670+satwikakota@users.noreply.github.com>
2020-07-26 23:08:11 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var car, wall; 
+function setup() {
+  createCanvas(800,400);
+  car= createSprite(50, 200, 50, 50);
+  speed=random(55,90); 
+  weight=random(400,1500); 
+  wall=createSprite(400,200,60,height/2); 
+  wall.shapeColor="green"; 
+  car.velocityX=speed;
+}
+
+function draw() {
+  background(0);  
+  console.log(car.velocityX); 
+  if(wall.x-car.x < (car.width)/2+(wall.width)/2){
+    car.velocityX=0; 
+    var deformation=0.5 * weight * speed * speed/ 22500
+    if(deformation>180){
+      car.shapeColor=color(255,0,0); 
+    }
+    if(deformation<100){
+      car.shapeColor=color(0,255,0);
+    }
+    if(deformation<100 & deformation>180){
+      car.shapeColor=color(230,230,0); 
+    }
+  }
+  drawSprites();
+}
